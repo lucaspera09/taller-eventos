7f98b523f7839b9ff58db808d9b461b834aa190b
lucaspera09 <lucaspera.2001@gmail.com>
2023-08-13 21:44:03 -0300

    agregue el div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const miDiv = document.querySelector(".contenedor");
+
+    miDiv.addEventListener("click", function() {
+        alert('Hola! Soy el div');
+    });
+});
